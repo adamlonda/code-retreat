f84846967162abe7ae3e0a9bf40cfcc2f580e44f
Unknown <adamlonda@gmail.com>
2018-05-26 10:01:47 +0200

    R1

---
+++ C-sharp/C-sharp/AliveCell.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp
+{
+    public class AliveCell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            AliveCell c = obj as AliveCell;
+
+            if (c == null)
+                return base.Equals(obj);
+
+            return c.X == X && c.Y == Y;
+        }
+    }
+}
